30848ddea06987f685542dd9450cb42b6dddc848
2TFD <120046971+2TFD@users.noreply.github.com>
2025-03-17 16:51:50 +0200

    Update cfg.py

--- cfg.py
+++ cfg.py
@@ -1,5 +1,5 @@
-TOKEN = '7222241386:AAHMalmtUXKX0Olj7Vs7paNHRPUbjlqvXCg' #telegram token
-admins = 1329056338
-hf_token = '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' #huggingface token
-CrypToken = '291519:AAusY0aRNfCvvyGk5GB0h9RjuNTNd66XvXl' # token crypto bot
-FastServer = 'https://refined-alpaca-fully.ngrok-free.app' # website link
+TOKEN = '' #telegram token
+admins = 
+hf_token = '' #huggingface token
+CrypToken = '' # token crypto bot
+FastServer = '' # website link
